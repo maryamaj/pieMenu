30193dfb28de4b28ebe99fa49cd083a6cffeb523
Tommaso <tommaso.piazza@gmail.com>
2012-02-16 10:43:03 +0100

    Initial Commit

---
+++ pieMenu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  pieMenu
+//
+//  Created by Tommaso Piazza on 2/16/12.
+//  Copyright (c) 2012 ChalmersTH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
